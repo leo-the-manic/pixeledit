cd7cb0b412b9983fc236e4b455a437fb60bf8479
aaa <aaa>
2014-09-01 17:19:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Pixledit
+========
+
+**Note:** This is a personal toy project.
+
+A simple image editor geared towards pixel art. Created with JavaFX.
+
+Current features (subject to change):
+
+* you can put red pixels on a white background
+* dragging the left mouse button across the canvas creates a stream of
+  disconnected red pixels
+* the background is 300x275 px
+* you can save that creation to a file called "image.png"
+
+Contributing
+------------
+How did you even get here!? Get outta my private repo you hacker/BitBucket
+employee
---
+++ src/sample/Controller.java
@@ -0,0 +1,69 @@
+package sample;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Controller {
+
+    private WritableImage image;
+
+    @FXML
+    private Canvas imageView;
+
+    @FXML
+    private ColorPicker colorPicker;
+
+    private void drawImage() {
+        GraphicsContext g = imageView.getGraphicsContext2D();
+        g.drawImage(image, 0, 0);
+    }
+
+    @FXML
+    public void initialize() {
+        image = new WritableImage(300, 275);
+        PixelWriter writer = image.getPixelWriter();
+        for(int x = 0; x < image.getWidth(); x++) {
+            for(int y = 0; y < image.getHeight(); y++) {
+                writer.setColor(x, y, Color.WHITE);
+            }
+        }
+        drawImage();
+    }
+
+    @FXML
+    public void useActiveTool(MouseEvent evt) {
+        // collect input
+        PixelWriter writer = image.getPixelWriter();
+        int x = (int)evt.getX();
+        int y = (int)evt.getY();
+        Color selectedColor = colorPicker.getValue();
+
+        // apply input
+        writer.setColor(x, y, selectedColor);
+        drawImage();
+    }
+
+    @FXML
+    public void saveImage(ActionEvent evt) throws IOException {
+        System.out.println("Saving...");
+        BufferedImage bufferedImage = SwingFXUtils.fromFXImage(image, null);
+        File outFile = new File("image.png");
+        ImageIO.write(bufferedImage, "png", outFile);
+        System.out.println("Wrote " + outFile);
+    }
+
+}
